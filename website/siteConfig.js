e695cb3a324bba67e8c93c17178068b54b7b4629
Cam Moore <cammymoore07@gmail.com>
2018-05-21 12:39:13 -1000

    Checkpoint

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,6 +40,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
+    { doc: 'why', label: 'Documentation'},
+    { blog: true, label: 'News' }
   ],
 
   // If you have users set above, you add it here:
